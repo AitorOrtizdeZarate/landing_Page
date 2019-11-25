77fa27b3639c6a9cf8a72c51f22656fa2621ed46
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-11-25 08:31:32 +0100

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+       <nav>
+           <div class="col-12 bg-dark">
+               <h1>Esturi</h1>
+           </div>
+       </nav>
+
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
