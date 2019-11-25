628f470eadecb38b3de78c19724b78913ffc0cd6
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-11-25 09:35:54 +0100

    me queda en formulario y hacer algo en el menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/css.css">
 
         <title>Laravel</title>
     </head>
     <body>
        <nav>
-           <div class="col-12 bg-dark">
-               <h1>Esturi</h1>
+           <div class="col-12 bg-warning" id="header">
+                <div id="logo">
+                     <img src="imagenes/logo.png" id="logoimg">
+                </div>
+                <div id="titulo">
+                    <h1>Esturi</h1>
+               </div>
+               <div id="menu">
+                   menu
+               </div>
            </div>
        </nav>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46453.28084427487!2d-2.008902497281466!3d43.307349767099105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd51affe3b68fe15%3A0xe43ec55994864649!2zU2FuIFNlYmFzdGnDoW4sIEd1aXDDunpjb2E!5e0!3m2!1ses!2ses!4v1574669358895!5m2!1ses!2ses" frameborder="0" style="border:0;" allowfullscreen="" id="mapa"></iframe>
+
+       
 
        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+       <footer>
+           <div class="col-12 bg-warning" id="footer">
+               <ul>
+                   <li>Twitter</li>
+                   <li>Facebook</li>
+                   <li>Instagram</li>
+                   <li>Google +</li>
+               </ul>
+           </div>
+       </footer>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     </body>
